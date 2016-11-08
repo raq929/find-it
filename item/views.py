dd106fca4275a7aecf9ec7f3ec8d0d76df30efa4
racheltstevens <racheltstevens@gmail.com>
2016-11-07 23:05:20 -0500

    Move search helpers to separate file

--- item/views.py
+++ item/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import View
-import re
-from django.db.models import Q
 
+from .search import get_query, normalize_query
 from .models import Item, Place, Room
 
 class ItemList(View):
@@ -46,41 +45,6 @@ def room_detail(request, slug):
 
 
 # search
-
-def normalize_query(query_string,
-                    findterms=re.compile(r'"([^"]+)"|(\S+)').findall,
-                    normspace=re.compile(r'\s{2,}').sub):
-    ''' Splits the query string in invidual keywords, getting rid of unecessary spaces
-        and grouping quoted words together.
-        Example:
-
-        >>> normalize_query('  some random  words "with   quotes  " and   spaces')
-        ['some', 'random', 'words', 'with quotes', 'and', 'spaces']
-
-    '''
-    return [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(query_string)]
-
-def get_query(query_string, search_fields):
-    ''' Returns a query, that is a combination of Q objects. That combination
-        aims to search keywords within a model by testing the given search fields.
-
-    '''
-    query = None # Query to search for every search term
-    terms = normalize_query(query_string)
-    for term in terms:
-        or_query = None # Query to search for a given term in each field
-        for field_name in search_fields:
-            q = Q(**{"%s__icontains" % field_name: term})
-            if or_query is None:
-                or_query = q
-            else:
-                or_query = or_query | q
-        if query is None:
-            query = or_query
-        else:
-            query = query & or_query
-    return query
-
 
 def search(request):
     query_string = ''
