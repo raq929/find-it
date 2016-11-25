579fb4e852a3f8133e2af7793c94a831806ed0db
racheltstevens <racheltstevens@gmail.com>
2016-11-25 17:19:25 -0500

    Implement first and last page in pagination
    * wrote scss for displaying pagination

--- item/utils.py
+++ item/utils.py
@@ -3,19 +3,34 @@ class PageLinksMixin:
    for previous and next pages"""
   page_kwarg = 'page'
 
+  def first_page(self, page):
+    # don't show on first page
+    if page.number > 1:
+      return self._page_urls(1)
+    return None
+
+  def last_page(self, page):
+      last_page = page.paginator.num_pages
+      if page.number < last_page:
+        return self._page_urls(last_page)
+      return None
+
   def _page_urls(self, page_number):
     return "?{pkw}={n}".format(
       pkw=self.page_kwarg,
       n=page_number)
 
   def previous_page(self, page):
-    if page.has_previous():
+    if (page.has_previous()
+            and page.number > 2):
       return self._page_urls(
         page.previous_page_number())
     return None
 
   def next_page(self, page):
-    if page.has_next():
+    last_page = page.paginator.num_pages
+    if (page.has_next()
+          and page.number < last_page - 1):
       return self._page_urls(
         page.next_page_number())
     return None
@@ -25,6 +40,10 @@ class PageLinksMixin:
     page = context.get('page_obj')
     if page is not None:
       context.update({
+          'first_page_url':
+            self.first_page(page),
+          'last_page_url':
+            self.last_page(page),
           'previous_page_url':
             self.previous_page(page),
           'next_page_url':
