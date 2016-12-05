9d0590e1d6983126c7fc8f06d85955f5fcfacb8b
racheltstevens <racheltstevens@gmail.com>
2016-12-04 21:35:26 -0500

    Implement save override for room form.
    Adds the house object

--- item/forms.py
+++ item/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from django.forms.widgets import HiddenInput
 
 from .models import Room, Place, Item, House
 from user.models import Profile
@@ -16,9 +17,22 @@ class SlugCleanMixin:
     return new_slug
 
 class RoomForm(SlugCleanMixin, forms.ModelForm):
+
+  def save(self, **kwargs):
+    house_obj = kwargs.get('house_obj', None)
+    if house_obj is not None:
+      instance = super().save(commit=False)
+      instance.house = house_obj
+      instance.save()
+      self.save_m2m()
+    else:
+      instance = super().save()
+    return instance
+
   class Meta:
     model = Room
     fields = '__all__'
+    widgets = {'house': HiddenInput()}
 
 class PlaceForm(SlugCleanMixin, forms.ModelForm):
   class Meta:
@@ -34,7 +48,6 @@ class HouseForm(SlugCleanMixin, forms.ModelForm):
   def __init__(self, *args, **kwargs):
     if 'user' in kwargs:
       self.user = kwargs.pop('user')
-    # print(**kwargs)
     super(HouseForm, self).__init__(*args, **kwargs)
 
   def save(self, **kwargs):
