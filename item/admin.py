4b0156438f5b1c3568a58906e029b0e984567c5d
racheltstevens <racheltstevens@gmail.com>
2016-12-31 11:31:41 -0500

    Register item models with admin site.
    Display house for places.
    Prepopulate slug field when creating places.

--- item/admin.py
+++ item/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import House, Room, Place, Item
 
 # Register your models here.
+admin.site.register(House)
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+  list_display = ('name', 'house')
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+  list_display = ('name', 'room', 'house')
+  search_fields = ('name', 'description')
+  prepopulated_fields = { "slug": ("name",)  }
+
+  def house(self, place):
+    return place.room.house
+
+@admin.register(Item)
+class PlaceAdmin(admin.ModelAdmin):
+  list_display = ('name', 'place')
