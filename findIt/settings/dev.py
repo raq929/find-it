1513afe61e09a4623ecf2f650a36406ff5d07d9f
racheltstevens <racheltstevens@gmail.com>
2017-01-01 16:45:23 -0500

    Split settings into base, dev, production

---
+++ findIt/settings/dev.py
@@ -0,0 +1,45 @@
+from base import *
+
+DEBUG = True
+
+SECRET_KEY = '$SECRET_KEY'
+
+ALLOWED_HOSTS = []
+
+
+INSTALLED_APPS += {
+  'debug_toolbar',
+  'django_extentions',
+}
+
+MIDDLEWARE_CLASSES.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
+
+# debug toolbar
+DEBUG_TOOLBAR_PATCH_SETTINGS = False
+INTERNAL_IPS = ['127.0.0.1']
+
+# Caches
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
+
+CACHE_MIDDLEWARE_ALIAS = 'default'
+
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'findit',
+        'USER': 'racheltstevens',
+        'PASSWORD': '$PASSWORD',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+# Email
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
---
+++ findIt/settings/production.py
@@ -0,0 +1,30 @@
+import os
+
+import dj_database_url
+
+from .base import *
+
+DEBUG = False
+TEMPLATE_INDEX = False
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+ALLOWED_HOSTS = [*]
+
+SECURE_PROXY_SSL_SERVER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+EMAIL_HOST = os.environ.get('POSTMARK_SMTP_SERVER')
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('POSTMARK_API_TOKEN')
+EMAIL_HOST_PASSWORD = os.environ.get('POSTMARK_API_TOKEN')
+EMAIL_USE_TLS = True
+
+TEMPLATES[0]['OPTIONS']['loaders'] = [
+    ('django.template.loaders.cached.Loader', [
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    ]),
+]
+
