cb5ec7ccdef793c72fd0408cb7021a3e805a41d8
racheltstevens <racheltstevens@gmail.com>
2017-01-26 11:33:31 -0500

    Modify house_list
    Take a can_edit argument passed by the template
    Add links to add residents or visitors

--- core/templatetags/house_list.py
+++ core/templatetags/house_list.py
@@ -12,13 +12,16 @@ register = Library()
 def house_list(context, *args, **kwargs):
     houses = (args[0] if len(args) > 0
               else kwargs.get('houses'))
+    can_edit = (args[1] if len(args) > 1
+          else kwargs.get('can_edit'))
 
     if houses is None:
         raise TemplateSyntaxError(
           "form template tag requires "
-          "at least one argument: houses. "
+          "at least one argument: houses, "
           "which is a list.")
 
     return {
         'houses': houses,
+        'can_edit': can_edit
     }
